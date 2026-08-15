54fb0e680ea47f4e67e65c312814ed9e3eb4c66a
padmanabhan720 <95848129+padmanabhan720@users.noreply.github.com>
2026-08-15 17:03:18 -0600

    Add files via upload

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MapScreen from './src/screens/MapScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen name="Map" component={MapScreen} options={{ title: 'MapNav' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
